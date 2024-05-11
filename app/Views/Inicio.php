01e49958d305af669d41b3c9acc77ed996cc634b
Alejandro MM <61571090+Zuldaar@users.noreply.github.com>
2024-05-11 13:58:11 -0700

    Readme y Stylesheet

--- app/Views/Inicio.php
+++ app/Views/Inicio.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
+    <link rel="stylesheet" href="<?= base_url('style.css') ?>">
 </head>
 <body>
     <h1>Busqueda API Marvel</h1>
--- app/Views/MarvelView.php
+++ app/Views/MarvelView.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Busqueda Marvel</title>
+    <link rel="stylesheet" href="<?= base_url('style.css') ?>">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 </head>
 <body>
